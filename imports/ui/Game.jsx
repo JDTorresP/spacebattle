1a58cd389580d9530651b89f5184851c8d5fb6cf
JDTorresP <jd.torres11@uniandes.edu.co>
2017-10-06 00:12:08 -0500

    redistribution of principal

---
+++ imports/ui/Game.jsx
@@ -0,0 +1,250 @@
+import React, { Component } from 'react';
+import Ship from '../sketches/Ship';
+import { randomNumBetweenExcluding } from '../sketches/helpers'
+
+//Definicion de constantes de las teclas segun navegador 
+//http://www.javascripter.net/faq/keycodes.htm
+const KEY = {
+  A: 65,
+  W: 87,
+  D: 68,
+  SPACE: 32
+};
+export default class Game extends Component {
+
+   constructor() {
+    super();
+    this.state = {
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.devicePixelRatio || 1,
+      },
+      context: null,
+      keys : {
+        left  : 0,
+        right : 0,
+        up    : 0,
+        down  : 0,
+        space : 0,
+      },
+      currentScore: 0,
+      topScore: localStorage['topscore'] || 0,
+      inGame: false
+    }
+    this.ship = [];
+    this.bullets = [];
+    this.particles = [];
+  }
+    
+handleResize(value, e){
+    this.setState({
+      screen : {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        ratio: window.devicePixelRatio || 1,
+      }
+    });
+  }
+handleKeys(value, e){
+    let keys = this.state.keys;
+    if(e.keyCode === KEY.A) keys.left  = value;
+    if(e.keyCode === KEY.D) keys.right = value;
+    if(e.keyCode === KEY.W) keys.up    = value;
+    if(e.keyCode === KEY.SPACE) keys.space = value;
+    this.setState({
+      keys : keys
+    });
+  }
+componentDidMount() {
+    window.addEventListener('keyup',   this.handleKeys.bind(this, false));
+    window.addEventListener('keydown', this.handleKeys.bind(this, true));
+    window.addEventListener('resize',  this.handleResize.bind(this, false));
+
+    const context = this.refs.canvas.getContext('2d');
+    this.setState({ context: context });
+    this.startGame();
+    requestAnimationFrame(() => {this.update()});
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleKeys);
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  update() {
+    const context = this.state.context;
+    const keys = this.state.keys;
+    const ship = this.ship[0];
+
+    context.save();
+    context.scale(this.state.screen.ratio, this.state.screen.ratio);
+
+    context.fillStyle = '#1a237e';
+    context.globalAlpha = 0.4;
+    context.fillRect(0, 0, this.state.screen.width, this.state.screen.height);
+    context.globalAlpha = 1;
+
+    // Check for colisions
+    // this.checkCollisionsWith(this.bullets, this.ship);
+
+    // Remove or render
+    this.updateObjects(this.particles, 'particles')
+    this.updateObjects(this.bullets, 'bullets')
+    this.updateShips('ship')
+
+    context.restore();
+
+    // Next frame
+    requestAnimationFrame(() => {this.update()});
+  }
+
+   updateObjects(items, group){
+    let index = 0;
+    for (let item of items) {
+      if (item.delete) {
+        this[group].splice(index, 1);
+      }else{
+        items[index].render(this.state);
+      }
+      index++;
+    }
+  }
+
+   updateShips(group){
+    let index = 0;
+    let items = this.props.ships;
+    for (let item of items) {
+      if (item.delete) {
+        this[group].splice(index, 1);
+      }else{
+        items[index].render(this.state);
+      }
+      index++;
+    }
+  }
+
+  addScore(points){
+    if(this.state.inGame){
+      this.setState({
+        currentScore: this.state.currentScore + points,
+      });
+    }
+  }
+
+  startGame(){
+    this.setState({
+      inGame: true,
+      currentScore: 0,
+    });
+
+    //Crea la nave
+    let ship = new Ship({
+      position: {
+        x: randomNumBetweenExcluding(0, this.state.screen.width, this.state.screen.width/2-60, this.state.screen.height/2+60),
+        y: randomNumBetweenExcluding(0, this.state.screen.height, this.state.screen.height/2-60, this.state.screen.width/2+60)  
+      },
+      create: this.createShip.bind(this),
+      onDie: this.gameOver.bind(this)
+    });
+    this.createShip(ship);
+
+  }
+
+  gameOver(){
+    this.setState({
+      inGame: false,
+    });
+
+    // Replace top score
+    if(this.state.currentScore > this.state.topScore){
+      this.setState({
+        topScore: this.state.currentScore,
+      });
+      localStorage['topscore'] = this.state.currentScore;
+    }
+  }
+
+  createShip(s)
+{
+     Ships.insert({
+      s,
+      createdAt: new Date(), // current time
+    });
+    this['ship'].push(s);
+}
+
+  createObject(item, group){
+    this[group].push(item);
+  }
+
+
+  checkCollisionsWith(items1, items2) {
+    var a = items1.length - 1;
+    var b;
+    for(a; a > -1; --a){
+      b = items2.length - 1;
+      for(b; b > -1; --b){
+        var item1 = items1[a];
+        var item2 = items2[b];
+        if(this.checkCollision(item1, item2)){
+          item1.destroy();
+          item2.destroy();
+        }
+      }
+    }
+  }
+
+  checkCollision(obj1, obj2){
+    var vx = obj1.position.x - obj2.position.x;
+    var vy = obj1.position.y - obj2.position.y;
+    var length = Math.sqrt(vx * vx + vy * vy);
+    if(length < obj1.radius + obj2.radius){
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+
+ let endgame;
+    let message;
+
+    if (this.state.currentScore <= 0) {
+      message = '0 points... So sad.';
+    } else if (this.state.currentScore >= this.state.topScore){
+      message = 'Top score with ' + this.state.currentScore + ' points. Woo!';
+    } else {
+      message = this.state.currentScore + ' Points though :)'
+    }
+
+    if(!this.state.inGame){
+      endgame = (
+        <div className="endgame">
+          <p>Game over!!</p>
+          <p>{message}</p>
+          <button
+            onClick={ this.startGame.bind(this) }>
+            Respawn
+          </button>
+        </div>
+      )
+    }
+
+    return (
+       <div>
+        { endgame }
+        <span className="score current-score" >Score: {this.state.currentScore}</span>
+        <span className="score top-score" >Top Score: {this.state.topScore}</span>
+        <span className="controls" >
+          Use [A][S][W][D] to MOVE<br/>
+          Use [SPACE] to SHOOT
+        </span>
+        <canvas ref="canvas"
+          width={this.state.screen.width * this.state.screen.ratio}
+          height={this.state.screen.height * this.state.screen.ratio}
+        />
+      </div>
+    );
+  }
+}
---
+++ imports/ui/Login.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="login">
+                <h2>Juegaaa!</h2>
+                <p>Porfa juegaaa</p>
+
+            </div>
+        );
+    }
+}
